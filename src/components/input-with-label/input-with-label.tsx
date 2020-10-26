08160453667f5eb2c9371a9a83a8bcb9defe82ba
Aldrin Tingson <aldrintingson@gmail.com>
2020-10-26 19:49:32 +0800

    simplify input-with-label in preparation for reuse in create wallet screen

--- src/components/input-with-label/input-with-label.tsx
+++ src/components/input-with-label/input-with-label.tsx
@@ -6,14 +6,12 @@ export interface InputWithLabelProps {
 }
 
 export const InputWithLabel: React.FC<InputWithLabelProps> = ({ id, title }) => (
-    <div className="-mx-3 md:flex mb-6">
-        <div className="md:w-full px-3">
-            <label className="block uppercase tracking-wide text-xs font-bold mb-2" htmlFor={id}>
-                {title}
-            </label>
-            <input
-                className="appearance-none block w-full bg-gray-100 text-gray-600 border border-gray-600 rounded py-3 px-4"
-                id={id} type="text"/>
-        </div>
+    <div className="md:w-full px-3 mb-2">
+        <label className="block uppercase tracking-wide text-xs font-bold mb-2" htmlFor={id}>
+            {title}
+        </label>
+        <input
+            className="appearance-none block w-full bg-gray-100 text-gray-600 border border-gray-600 rounded py-3 px-4"
+            id={id} type="text"/>
     </div>
 )
