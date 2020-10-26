53c545ec328d30b1b7a63457ebbf57ec310de82f
Aldrin Tingson <aldrintingson@gmail.com>
2020-10-26 19:46:54 +0800

    minor responsive fix for wallet-item card

--- src/components/wallet-item/wallet-item.tsx
+++ src/components/wallet-item/wallet-item.tsx
@@ -12,7 +12,7 @@ export const WalletItem: React.FC<WalletItemProps> = ({ walletAddress, darkBalan
         walletAddress.substring(length - 4, length)
 
     return (
-        <div className="w-full md:w-1/3 px-2">
+        <div className="w-full md:w-1/3 px-2 py-2 md:py-0">
             <div className="rounded-lg shadow-sm">
                 <div className="rounded-lg bg-white shadow-lg md:shadow-xl relative overflow-hidden">
                     <div className="px-3 pt-8 pb-10 text-center relative z-10">
