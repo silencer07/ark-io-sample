af094e90fcf3ea6ead7978e1c2066bd81a09ff18
Aldrin Tingson <aldrintingson@gmail.com>
2020-10-27 09:55:46 +0800

    add save to local storage functionality

--- src/model/root-store/setup-root-store.ts
+++ src/model/root-store/setup-root-store.ts
@@ -1,25 +1,21 @@
 import {RootStoreModel, RootStore} from "./root-store"
+import {onSnapshot} from "mobx-state-tree";
+
+const ROOT_STATE_STORAGE_KEY = "ark-io-data"
 
 export async function setupRootStore() {
   let rootStore: RootStore
-  let data: any
+  let data: any = localStorage.getItem("ROOT_STATE_STORAGE_KEY") || {}
 
   try {
-    // load data from storage
-    // data = (await storage.load(ROOT_STATE_STORAGE_KEY)) || {}
-    data = {}
     rootStore = RootStoreModel.create(data)
   } catch (e) {
-    // if there's any problems loading, then let's at least fallback to an empty state
-    // instead of crashing.
     rootStore = RootStoreModel.create({})
-
-    // but please inform us what happened
     console.error(e.message, null)
   }
 
   // track changes & save to storage
-  // onSnapshot(rootStore, (snapshot) => storage.save(ROOT_STATE_STORAGE_KEY, snapshot))
+  onSnapshot(rootStore, (snapshot) => localStorage.setItem(ROOT_STATE_STORAGE_KEY, JSON.stringify(snapshot)))
 
   return rootStore
 }
